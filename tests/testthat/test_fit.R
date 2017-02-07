db6eab61d6dd84214a3c5afa3c342fd6ca059e5e
Hansjoerg Plieninger <plieninger@uni-mannheim.de>
2017-02-07 16:53:04 +0100

    added tests

--- tests/testthat/test_fit.R
+++ tests/testthat/test_fit.R
@@ -69,22 +69,86 @@ test_that("generate_irtree() returns correct output", {
 
 context("Model fitting")
 
-res1 <- fit_irtree(dat1$X, revItem = dat1$revItem,
-                   M = 100, warmup = 100, n.chains = 1,
-                   fitModel = "2012", fitMethod = "jags")
-res2 <- fit_irtree(dat1$X, revItem = dat1$revItem,
-                   M = 100, warmup = 100, n.chains = 1,
-                   fitModel = "ext", fitMethod = "stan")
-res3 <- fit_irtree(dat2$X, revItem = dat1$revItem,
-                   M = 100, warmup = 100, n.chains = 1,
-                   fitModel = "ext", fitMethod = "jags")
-res4 <- fit_irtree(dat2$X, revItem = dat1$revItem,
-                   M = 100, warmup = 100, n.chains = 1,
-                   fitModel = "2012", fitMethod = "stan")
+M <- 100
+
+invisible(capture.output(
+    res1 <- fit_irtree(dat1$X, revItem = dat1$revItem,
+                       M = M, warmup = 100, n.chains = 1,
+                       fitModel = "2012", fitMethod = "jags"),
+    res2 <- fit_irtree(dat1$X, revItem = dat1$revItem,
+                       M = M, warmup = 100, n.chains = 1,
+                       fitModel = "ext", fitMethod = "stan"),
+    res3 <- fit_irtree(dat2$X, revItem = dat1$revItem,
+                       M = M, warmup = 100, n.chains = 1,
+                       fitModel = "ext", fitMethod = "jags"),
+    res4 <- fit_irtree(dat2$X, revItem = dat1$revItem,
+                       M = M, warmup = 100, n.chains = 1,
+                       fitModel = "2012", fitMethod = "stan")
+))
+
+test_that("fit_irtree() returns MCMC list", {
+    expect_equal(unique(sapply(res1$samples$mcmc, nrow)), M)
+    expect_equal(unique(sapply(rstan::As.mcmc.list(res2$samples), nrow)), M)
+    expect_equal(unique(sapply(res3$samples$mcmc, nrow)), M)
+    expect_equal(unique(sapply(rstan::As.mcmc.list(res4$samples), nrow)), M)
+})
+
+# res1 <- fit_irtree(dat1$X, revItem = dat1$revItem,
+#                    M = 100, warmup = 100, n.chains = 1,
+#                    fitModel = "2012", fitMethod = "jags")
+# res2 <- fit_irtree(dat1$X, revItem = dat1$revItem,
+#                    M = 100, warmup = 100, n.chains = 1,
+#                    fitModel = "ext", fitMethod = "stan")
+# res3 <- fit_irtree(dat2$X, revItem = dat1$revItem,
+#                    M = 100, warmup = 100, n.chains = 1,
+#                    fitModel = "ext", fitMethod = "jags")
+# res4 <- fit_irtree(dat2$X, revItem = dat1$revItem,
+#                    M = 100, warmup = 100, n.chains = 1,
+#                    fitModel = "2012", fitMethod = "stan")
+
+
+# SUMMARIZING MODEL RESULTS -----------------------------------------------
+
+context("Summarizing fitted models")
 
 
 
+res1b <- summarize_irtree_fit(res1)
+res1c <- tidyup_irtree_fit(res1b, N = N, J = J, revItem = dat1$revItem,
+                           traitItem = dat1$traitItem, fitModel = res1$fitModel,
+                           fitMethod = res1$fitMethod)
+res2b <- summarize_irtree_fit(res2)
+res2c <- tidyup_irtree_fit(res2b, N = N, J = J, revItem = dat1$revItem,
+                           traitItem = dat1$traitItem, fitModel = res2$fitModel,
+                           fitMethod = res2$fitMethod)
+res3b <- summarize_irtree_fit(res3)
+res3c <- tidyup_irtree_fit(res3b, N = N, J = J, revItem = dat2$revItem,
+                           traitItem = dat2$traitItem, fitModel = res3$fitModel,
+                           fitMethod = res3$fitMethod)
+res4b <- summarize_irtree_fit(res4)
+res4c <- tidyup_irtree_fit(res4b, N = N, J = J, revItem = dat2$revItem,
+                           traitItem = dat2$traitItem, fitModel = res4$fitModel,
+                           fitMethod = res4$fitMethod)
+unique(sapply(res1c$beta, nrow))
 
-# res2b <- summarize_irtree_fit(res2)
-# res2c <- tidyup_irtree_fit(res2b, N = N, J = J, revItem = dat$revItem,
-#                            traitItem = dat$traitItem, fitModel = res1$fitModel)
+test_that("tidyup_irtree_fit() returns correlations", {
+    expect_equal(length(res1c$Corr), 4)
+    expect_equal(length(res2c$Corr), 4)
+    expect_equal(length(res3c$Corr), 4)
+    expect_equal(length(res4c$Corr), 4)
+    expect_equal(unique(as.vector(sapply(res1c$Sigma, dim))), 3)
+    expect_equal(unique(as.vector(sapply(res2c$Sigma, dim))), 4)
+    expect_equal(unique(as.vector(sapply(res3c$Sigma, dim))), 4)
+    expect_equal(unique(as.vector(sapply(res4c$Sigma, dim))), 3)
+})
+
+test_that("tidyup_irtree_fit() returns correct number of parameters", {
+    expect_equal(unique(sapply(res1c$beta, nrow)), J)
+    expect_equal(unique(sapply(res2c$beta, nrow)), J)
+    expect_equal(unique(sapply(res3c$beta, nrow)), J)
+    expect_equal(unique(sapply(res4c$beta, nrow)), J)
+    expect_equal(unique(sapply(res1c$theta, nrow)), N)
+    expect_equal(unique(sapply(res2c$theta, nrow)), N)
+    expect_equal(unique(sapply(res3c$theta, nrow)), N)
+    expect_equal(unique(sapply(res4c$theta, nrow)), N)
+})
