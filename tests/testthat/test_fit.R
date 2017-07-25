635db45ef69c2a03403fa0fb9244a5d3bc745277
Hansjoerg Plieninger <plieninger@uni-mannheim.de>
2017-07-25 15:13:09 +0200

    additional testthat-tests

--- tests/testthat/test_fit.R
+++ tests/testthat/test_fit.R
@@ -116,18 +116,33 @@ res1b <- summarize_irtree_fit(res1)
 res1c <- tidyup_irtree_fit(res1b, N = N, J = J, revItem = dat1$revItem,
                            traitItem = dat1$traitItem, fitModel = res1$fitModel,
                            fitMethod = res1$fitMethod)
+res1d <- suppressMessages(
+    pp_irtree(res1b$mcmc, iter = 10, N = N, traitItem = dat1$traitItem,
+              revItem = dat1$revItem, fitModel = res1$fitModel))
+
 res2b <- summarize_irtree_fit(res2)
 res2c <- tidyup_irtree_fit(res2b, N = N, J = J, revItem = dat1$revItem,
                            traitItem = dat1$traitItem, fitModel = res2$fitModel,
                            fitMethod = res2$fitMethod)
+res2d <- suppressMessages(
+    pp_irtree(res2b$mcmc, iter = 10, N = N, traitItem = dat1$traitItem,
+              revItem = dat1$revItem, fitModel = res2$fitModel))
+
 res3b <- summarize_irtree_fit(res3)
 res3c <- tidyup_irtree_fit(res3b, N = N, J = J, revItem = dat2$revItem,
                            traitItem = dat2$traitItem, fitModel = res3$fitModel,
                            fitMethod = res3$fitMethod)
+res3d <- suppressMessages(
+    pp_irtree(res3b$mcmc, iter = 10, N = N, traitItem = dat1$traitItem,
+              revItem = dat1$revItem, fitModel = res3$fitModel))
+
 res4b <- summarize_irtree_fit(res4)
 res4c <- tidyup_irtree_fit(res4b, N = N, J = J, revItem = dat2$revItem,
                            traitItem = dat2$traitItem, fitModel = res4$fitModel,
                            fitMethod = res4$fitMethod)
+res4d <- suppressMessages(
+    pp_irtree(res4b$mcmc, iter = 10, N = N, traitItem = dat1$traitItem,
+              revItem = dat1$revItem, fitModel = res4$fitModel))
 
 test_that("tidyup_irtree_fit() returns correlations", {
     expect_equal(length(res1c$Corr), 4)
@@ -149,6 +164,38 @@ test_that("tidyup_irtree_fit() returns correct number of parameters", {
     expect_equal(unique(sapply(res2c$theta, nrow)), N)
     expect_equal(unique(sapply(res3c$theta, nrow)), N)
     expect_equal(unique(sapply(res4c$theta, nrow)), N)
+})
+
+test_that("pp_irtree() returns valid values", {
+    expect_is(res1d, "data.frame")
+    expect_is(res2d, "data.frame")
+    expect_is(res3d, "data.frame")
+    expect_is(res4d, "data.frame")
+    
+    expect_equal(as.numeric(levels(res1d$Item)), 1:J)
+    expect_equal(as.numeric(levels(res2d$Item)), 1:J)
+    expect_equal(as.numeric(levels(res3d$Item)), 1:J)
+    expect_equal(as.numeric(levels(res4d$Item)), 1:J)
+    
+    expect_equal(as.numeric(levels(res1d$Categ)), 1:5)
+    expect_equal(as.numeric(levels(res2d$Categ)), 1:5)
+    expect_equal(as.numeric(levels(res3d$Categ)), 1:5)
+    expect_equal(as.numeric(levels(res4d$Categ)), 1:5)
+    
+    expect_equal(ncol(res1d), 6)
+    expect_equal(ncol(res2d), 6)
+    expect_equal(ncol(res3d), 6)
+    expect_equal(ncol(res4d), 6)
+    
+    expect_gte(min(res1d[, 3:6]), 0)
+    expect_gte(min(res2d[, 3:6]), 0)
+    expect_gte(min(res3d[, 3:6]), 0)
+    expect_gte(min(res4d[, 3:6]), 0)
+    
+    expect_lte(min(res1d[, 3:6]), 1)
+    expect_lte(min(res2d[, 3:6]), 1)
+    expect_lte(min(res3d[, 3:6]), 1)
+    expect_lte(min(res4d[, 3:6]), 1)
 })
 
 
